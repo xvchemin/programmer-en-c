c3b30e124de53555020dda9f3fe3124474f28feb
Xavier CHEMIN <xv.chemin@gmail.com>
2025-01-16 22:50:33 +0100

    P1C5.3 les HashSet

--- P1/P1C5.3/Program.cs
+++ P1/P1C5.3/Program.cs
@@ -5,10 +5,10 @@ ingredients.Add("beurre");
 ingredients.Add("vanille");
 
 // TODO : ajouter un autre ingrédient à l'ensemble
-
+ingredients.Add("cyanure");
 
 // TODO : retirer la vanille de l'ensemble
-
+ingredients.Remove("chocolat");
 
 // Afficher la liste des ingrédients
 Console.WriteLine("Voici la liste de nos ingrédients");
