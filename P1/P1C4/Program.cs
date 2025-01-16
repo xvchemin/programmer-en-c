814b33005078a0e7c54c3e3e71ff6d06fa24cdd9
fny-plx <fpoullieux@gmail.com>
2025-01-16 22:01:26 +0100

    instanciation d'un objet et attribution valeur aux attributs

--- P1/P1C4/Program.cs
+++ P1/P1C4/Program.cs
@@ -1,8 +1,14 @@
 ﻿// TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
+using P1C4;
+
+Livre monLivre = new Livre();
 
 // TODO, étape 2 : affecter une valeur aux champs titre, auteur et nombre de pages de votre objet
+monLivre.Titre = "Le Comte de Monte-Cristo";
+monLivre.Auteur = "Alexandre Dumas";
+monLivre.NombreDePages = 1889;
 
 // Afficher le contenu des champs de cet objet 
 Console.WriteLine("Le titre du livre est " + monLivre.Titre);
 Console.WriteLine("Son auteur est " + monLivre.Auteur);
-Console.WriteLine("Il compte " + monLivre.NombreDePages);
+Console.WriteLine("Il compte " + monLivre.NombreDePages + " pages");
