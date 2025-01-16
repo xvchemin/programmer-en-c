f1e67a80d1a06a9e9a2910d37d1b370ac6f8ebe3
Xavier CHEMIN <xv.chemin@gmail.com>
2025-01-16 22:10:00 +0100

    P1C5.1 declaration et affectation array

--- P1/P1C5.1/Program.cs
+++ P1/P1C5.1/Program.cs
@@ -1,11 +1,15 @@
 ﻿// TODO : Déclarer une variable nommée couleurs d'un array de string de longueur 5
+using System.Text;
+using System;
 
+string[] couleurs;
 
 // TODO : Remplir le tableau avec les couleurs demandées dans le README.md
+couleurs = new string[5] { "rouge", "jaune", "orange", "vert", "bleu" };
 
 
 // TODO : Remplacer vert par émeraude dans le tableau
-
+couleurs[3] = "émeraude";
 
 // Afficher le contenu du tableau
 foreach (string couleur in couleurs)
