2316abe37624de18865695c794da92cd5684a5c7
fny-plx <fpoullieux@gmail.com>
2025-01-16 22:17:10 +0100

    array de chaine de caractères

--- P1/P1C5.1/Program.cs
+++ P1/P1C5.1/Program.cs
@@ -1,11 +1,12 @@
 ﻿// TODO : Déclarer une variable nommée couleurs d'un array de string de longueur 5
-
+string[] couleurs;
+couleurs = new string[5];
 
 // TODO : Remplir le tableau avec les couleurs demandées dans le README.md
-
+couleurs = new string[] {"rouge","jaune","orange","vert","bleu"};
 
 // TODO : Remplacer vert par émeraude dans le tableau
-
+couleurs[3] = "émeraude";
 
 // Afficher le contenu du tableau
 foreach (string couleur in couleurs)
