cc3140bb102f67685b38a36f6f07d79183e05c4b
Xavier CHEMIN <xv.chemin@gmail.com>
2025-01-22 22:05:47 +0100

    P2C3 les boucles

--- P2/P2C3/Program.cs
+++ P2/P2C3/Program.cs
@@ -1,1 +1,19 @@
 ﻿//TODO : Créer une boucle for et while qui affiche à 5 reprises la phrase, « Je m'amuse comme un fou ! ».
+Console.WriteLine("Bonjour !");
+
+/* for (int i = 0; i < 5; i++)
+     {
+         Console.WriteLine("Je m'amuse comme un fou !");
+     };
+*/
+
+int J = 0;
+
+while (J < 5)
+{
+    J++;
+
+    if (J == 4) {  continue; };
+
+    Console.WriteLine("Je m'amuse comme un fou !" + J);
+};
