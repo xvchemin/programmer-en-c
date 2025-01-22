941fe29257b4fca8b93cd397f1cf90dc388675aa
Xavier CHEMIN <xv.chemin@gmail.com>
2025-01-22 21:43:58 +0100

    P2C2.2 le switch

--- P2/P2C2.2/Program.cs
+++ P2/P2C2.2/Program.cs
@@ -1,25 +1,28 @@
 ﻿using P2C2._2;
 
-Couleur couleurChoisie = Couleur.Vert;
+Couleur couleurChoisie = Couleur.Rouge;
 
 // TODO : Remplacer la séquence if/else par une instruction switch équivalente qui fonctionne selon la valeur de couleurChoisie
-if (couleurChoisie == Couleur.Bleu)
+
+switch (couleurChoisie)
 {
-    Console.WriteLine("La couleur est bleue");
+    case Couleur.Bleu :
+        Console.WriteLine("La couleur est bleue");
+        break;
+    case Couleur.Vert :
+        Console.WriteLine("La couleur est verte");
+        break;  
+    case Couleur.Rouge :
+        Console.WriteLine("La couleur est rouge");
+        break;
+    case Couleur.Jaune :
+        Console.WriteLine("La couleur est jaune");
+        break;
+    default:
+        Console.WriteLine("Cette couleur n'est pas connue !");
+        break;
 }
-else if (couleurChoisie == Couleur.Vert)
-{
-    Console.WriteLine("La couleur est verte");
-}
-else if (couleurChoisie == Couleur.Rouge)
-{
-    Console.WriteLine("La couleur est rouge");
-}
-else if (couleurChoisie == Couleur.Jaune)
-{
-    Console.WriteLine("La couleur est jaune");
-}
-else
-{
-    Console.WriteLine("Cette couleur n'est pas connue !");
-}
+
+
+    
+
