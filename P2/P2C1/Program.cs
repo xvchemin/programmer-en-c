f9e7c3771143af6730ec93576102427d4b702025
Xavier CHEMIN <xv.chemin@gmail.com>
2025-01-22 21:10:49 +0100

    P2C1 Main

--- P2/P2C1/Program.cs
+++ P2/P2C1/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+namespace Hello
+{
+    /// <summary>
+    /// Ceci est une implémentation du message traditionnel « Hello world! »
+    /// </summary>
+    /// <remarks>Créée par l'équipe éducative d'OpenClassrooms</remarks>
+    public class HelloWorld
+    {
+        /// <summary>
+        /// Le programme commence ici
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            AfficheBonjour("Xavier");
+        }
+
+
+        private static void AfficheBonjour(string args )
+        {
+            Console.WriteLine("Hello " + args + "!");
+        }
+    }
+}
