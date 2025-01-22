a447b85a61f7d14f439bb76dab03c19cb3316897
fny-plx <fpoullieux@gmail.com>
2025-01-22 21:50:19 +0100

    P2C1 Hello

--- P2/P2C1/Program.cs
+++ P2/P2C1/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//Console.WriteLine("Bonjour le monde !");
+
+
+using System;
+
+namespace Hello
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            AfficheBonjour("Bonjour le monde !");
+        }
+        private static void AfficheBonjour(string phrase)
+        {
+            Console.WriteLine(phrase);
+        }
+    }
+}
