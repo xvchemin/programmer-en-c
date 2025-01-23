d40e5cba92c24e8f1f6fe9c50c88e54a3dfd925a
fny-plx <fpoullieux@gmail.com>
2025-01-23 21:26:42 +0100

    boucles for et while

--- P2/P2C3/Program.cs
+++ P2/P2C3/Program.cs
@@ -1,1 +1,23 @@
 ﻿//TODO : Créer une boucle for et while qui affiche à 5 reprises la phrase, « Je m'amuse comme un fou ! ».
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine("Je m'amuse comme un fou !");
+//}
+
+
+int x = 0;
+
+while (x < 5)
+{
+    x += 1;
+
+    if (x == 4)
+    {
+        continue;
+    }
+    Console.WriteLine("Je m'amuse comme un fou !");
+}
+
+Console.WriteLine("J'en ai marre !");
+
